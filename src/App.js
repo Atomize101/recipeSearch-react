0318fdb34cd18c82a2098b677a4acdde5fecfd2e
Chris Patrick <atomize101@gmail.com>
2020-10-26 11:05:02 -0400

    getRecipes async function to pull api data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 const keys = require('./config/dev');
 
 const App = () => {
-	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${keys.APP_ID}&app_key=${keys.APP_KEY}`;
+	useEffect(() => {
+		getRecipes();
+	}, []);
 
-	const [counter, setCounter] = useState(0);
+	const getRecipes = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/search?q=chicken&app_id=${keys.APP_ID}&app_key=${keys.APP_KEY}`
+		);
+		const data = await response.json();
+		console.log(data);
+	};
 
 	return (
 		<div className="App">
@@ -16,7 +24,6 @@ const App = () => {
 					Search
 				</button>
 			</form>
-			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
 		</div>
 	);
 };
