9501974a06a8e0c20e2e413026307af8e5a0244d
Chris Patrick <atomize101@gmail.com>
2020-10-26 10:51:03 -0400

    Getting some state setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const keys = require('./config/dev');
@@ -6,7 +6,19 @@ const keys = require('./config/dev');
 const App = () => {
 	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${keys.APP_ID}&app_key=${keys.APP_KEY}`;
 
-	return <div className="App">Testing</div>;
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div className="App">
+			<form className="search-form">
+				<input className="search-bar" type="text" />
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+		</div>
+	);
 };
 
 export default App;
